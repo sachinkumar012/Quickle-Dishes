a7ee6151230307f18e844015dcf328f2f1720eef
Sachin Kumar <117492894+sachinkumar012@users.noreply.github.com>
2025-04-24 00:36:43 +0530

    Update orderController.js

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -3,9 +3,7 @@ import userModel from "../models/userModel.js"
 import Stripe from "stripe";
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
-const currency="inr";
-const deliveryCharge=50;
-const frontend_URL='https://quickle-dishes-frontend.onrender.com';
+ const frontend_URL='https://quickle-dishes-frontend.onrender.com';
 
 // Placing User Order for Frontend
 const placeOrder = async (req, res) => {
